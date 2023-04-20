9d9c333cd7585bddb14814c990668116681e0851
carlistosluo <carlistos.luo@outlook.com>
2023-04-21 01:24:01 +0800

    数据初始化及例子展示

--- mail.go
+++ mail.go
@@ -1,8 +1,13 @@
 package main
 
-import "ginchat/router"
+import (
+	"ginchat/router"
+	"ginchat/utils"
+)
 
 func main() {
+	utils.InitConfig()
+	utils.InitMySQL()
 	r := router.Router()
 
 	r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
